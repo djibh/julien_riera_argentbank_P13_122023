d007b0eda8677c349d617cc0fb755aaa3b565fa7
Djibh <julien.riera@protonmail.com>
2023-10-30 09:40:45 +0100

    pack (AUTH) > Install Axios

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/router.jsx';
+import { Provider } from 'react-redux';
+import store from './utils/store';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
